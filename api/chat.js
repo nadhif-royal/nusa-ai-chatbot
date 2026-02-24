1d316a26ed04ad581e890d2e1198a87029b75a02
Nadhif Rif'at <nadhifrifat@gmail.com>
2026-02-24 16:40:48 +0700

    Ganti model

--- api/chat.js
+++ api/chat.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
     const { message } = req.body;
     
     const model = genAI.getGenerativeModel({ 
-      model: "gemini-1.5-flash", 
+      model: "gemini-2.5-flash", 
       systemInstruction: `Kamu adalah NusaBot, representasi teknologi dari Nusa AI.
 
 Identitas & Kreator:
